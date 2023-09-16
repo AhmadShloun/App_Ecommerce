53ff401b502228fc9cca11d9c3eb9712bcedba59
Ahmad <kingsh201@gmai.com>
2023-09-16 15:42:46 +0300

    Final Favourite

--- lib/view/widget/myfavourite/custom_list_favourite_item.dart
+++ lib/view/widget/myfavourite/custom_list_favourite_item.dart
@@ -1,7 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:ecommerce/controller/favourite_controller/favourite_controller.dart';
 import 'package:ecommerce/controller/favourite_controller/myfavourite_controller.dart';
-import 'package:ecommerce/controller/items_controller/items_controller.dart';
 import 'package:ecommerce/core/constant/color.dart';
 import 'package:ecommerce/core/functions/translate_db.dart';
 import 'package:ecommerce/data/model/myfavouritemodel.dart';
